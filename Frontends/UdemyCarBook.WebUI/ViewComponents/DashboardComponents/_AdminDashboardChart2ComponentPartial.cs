3abad5c535572ec50c098f04de85ae4f0f08a9f0
MnrMine <mnr_mine_agri@hotmail.com>
2024-09-23 10:10:13 +0300

    Add project files.

---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/DashboardComponents/_AdminDashboardChart2ComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardChart2ComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
